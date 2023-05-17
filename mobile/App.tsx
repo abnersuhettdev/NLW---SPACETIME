3703e25b163dde6a37e814d639a35b20e882196a
Abner Suhett <suhettabner@gmail.com>
2023-05-17 19:02:04 -0300

    pagina inicial no mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,65 @@
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
+
+import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
+import { styled } from "nativewind";
+import NlwLogo from "./src/assets/logo.svg";
+import blurBg from "./src/assets/luz.png";
+import Stripes from "./src/assets/stripes.svg";
+
+const StyledStripes = styled(Stripes);
 
 export default function App() {
-	return (
-		<View className="bg-zinc-950 flex-1 items-center justify-center">
-			<Text className="text-zinc-50 font-bold text-6xl">Hello World</Text>
-			<StatusBar style="light" translucent />
-		</View>
-	);
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  });
+
+  if (!hasLoadedFonts) {
+    return null;
+  }
+
+  return (
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
+      imageStyle={{ position: "absolute", left: "-100%" }}
+    >
+      <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <NlwLogo />
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantaes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-3"
+        >
+          <Text className="font-alt text-sm uppercase leading-none text-black">
+            Cadastrar lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito por Abner durante a NLW da Rocketseat
+      </Text>
+
+      <StatusBar style="light" translucent />
+    </ImageBackground>
+  );
 }
